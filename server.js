02052dccd5446237777e7fa1ab94e7da8e55a894
midu-zack <midlajmidu0002@gmail.com>
2024-08-25 20:23:43 +0530

    latest

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
 // Routes
 app.get('/', (req, res) => {
